e0c6917f4305fc55842e2e27e0e93110494ed5f7
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-10-02 19:09:00 +0700

    change-commit-src-block-ts

--- src/block.ts
+++ src/block.ts
@@ -20,44 +20,32 @@ class Block {
       this.timestamp = timestamp;  
       this.prevHash = prevHash;  
 
-      this.hash = this.calHash(); 
-      this.nonce = 0; 
-    } 
-
-    calHash() {
-      return SHA256(
-         this.height + this.prevHash + this.timestamp + JSON.stringify(this.transactions + this.nonce
-    ).toString()
-  )};
-
-    mineBlock(difficulty) {
-      while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
-        this.nonce++;
-        this.hash = this.calHash();  
-      }     
-      console.log(`Mined Block: ${this.hash}`);  
-    }  
-
-    hasValidTransactions() {
-      for (const trans of this.transactions) {
-        if (!trans.isValidTransaction()) {
-          return false;  
-        } 
-      }
-      return true; 
-    }
+      this.hash = this.calcHash(); 
+      this.nonce = 0;
   }
 
+  calcHash() { 
+    return SHA256(
+      this.height + this.prevHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) { 
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      this.nonce++;
+      this.hash = this.calcHash();
+    }
+    console.log(`Mined Block: ${this.hash}`);
+  }
+
+  hasValidTransactions() { 
+    for (const trans of this.transactions) { 
+      if (!trans.isValidTransaction()) { 
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
 export default Block;
-function mineBlock(difficulty: any) {
-  throw new Error('Function not implemented.'); 
-}
-
-function hasValidTransactions() {
-  throw new Error('Function not Implemented.');  
-}
-
-
-
-
-
